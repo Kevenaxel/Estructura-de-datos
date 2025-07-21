3d07e92d8e2f37d2c315c2c2827bd670a726caad
Kevenaxel <hectorsastre476@gmail.com>
2025-07-21 10:10:36 -0600

    new

---
+++ Semana 2/Ejercicio1.py
@@ -0,0 +1,29 @@
+#Tipos de datos y rangos
+
+# entero = 10 ** 100      #Numero enteros int
+# print(entero)
+
+# print(type(entero))
+
+
+
+
+
+#import sys
+
+#print (sys.float_info.max)
+#print (sys.float_info.min)
+#print (sys.float_info.dig)
+
+
+
+#a = True
+
+#b = False 
+
+#print(int(b))
+
+# cadena = "a" * 1000
+
+# print(len(cadena))
+# print(type(cadena))
