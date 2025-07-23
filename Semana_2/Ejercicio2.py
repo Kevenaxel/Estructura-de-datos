28c4fba79254f4a84560964b964e26ee489ea878
Kevenaxel <hectorsastre476@gmail.com>
2025-07-23 09:52:57 -0600

    new

---
+++ Semana_2/Ejercicio2.py
@@ -0,0 +1,12 @@
+
+#Kevin Mauricio Alvarenga Flores
+
+#print("Hello word \n mi nombre es: \n Kevin Flores \n Carrera: Ingenieria en Desarrollo de Software")
+
+#print(" Nombre:Juan\n Apellido:Perez\n Celular:12321321\n Correo:juan.perez@univo.edu.sv ")
+
+#print("Nombre\tApellido\tCelular\tCorreo\nJuan\tPerez\t\t12321321\tjuan.perez@univo.edu.sv")
+
+#input("Ingrese su nombre")
+print("Hola, Bienvenido " + input("Ingrese su nombre: ") + " " +input("Ingrese sus apellidos: ")+ " " + input("Ingrese su carnet: ") +"@edu.sv")
+
