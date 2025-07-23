89b5527095c77cc4dcfc777686f5b3ce6faac61c
Kevenaxel <hectorsastre476@gmail.com>
2025-07-23 10:24:37 -0600

    new

--- Semana_2/Ejercicio2.py
+++ Semana_2/Ejercicio2.py
@@ -8,5 +8,12 @@
 #print("Nombre\tApellido\tCelular\tCorreo\nJuan\tPerez\t\t12321321\tjuan.perez@univo.edu.sv")
 
 #input("Ingrese su nombre")
-print("Hola, Bienvenido " + input("Ingrese su nombre: ") + " " +input("Ingrese sus apellidos: ")+ " " + input("Ingrese su carnet: ") +"@edu.sv")
+#print("Hola, Bienvenido " + input("Ingrese su nombre: ") + " " +input("Ingrese sus apellidos: ")+ " " + input("Ingrese su carnet: ") +"@edu.sv")
 
+#Declaracion de Variables
+
+nombre = "Kevin Flores"
+edad = 22
+estatura = 1.50
+
+print("Hola, Bienvenido " +"Su nombre es: " +nombre +" Su edad es: "+str(edad)  +" Su estatura es: " +str(estatura) )
