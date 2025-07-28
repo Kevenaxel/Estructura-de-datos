6ad185aec793cf31db8116a08e361c496783f70e
Kevenaxel <hectorsastre476@gmail.com>
2025-07-28 10:27:41 -0600

    new

---
+++ Semana_3/Ejercicio1.py
@@ -0,0 +1,55 @@
+#Declaracion de variables
+#Nombre = "Kevin Flores"
+#Edad = 20
+#Estatura = 1.75
+
+#print(f"Mi nombre : {Nombre} tengo la edad de: {Edad}, mido: {Estatura} metros")
+
+
+
+#Longitudes en carecteres
+#oracion1 = "El Clima es perfecto"
+#oracion2 = "Con frio"
+
+#print(oracion1)
+#print(oracion2)
+
+#print(len(oracion1+oracion2))
+#print(len(oracion2))
+
+#Crear una lista de estudiantes
+
+#estudiantes = ["Kevin", "Enmanuel", "Brayan", "Yony"]  #Creacion de lista
+#print (estudiantes[3]) #Imprimi "Kevin"
+
+#Agregar un nuevo estudiante
+#estudiantes.append("Pedro")
+
+
+#Eliminar un estudiante
+#estudiantes.remove("Yony")
+
+#Mostrar las listas actualizadas
+#print(estudiantes)
+
+#numeros = [1,2,3,4]
+#print(numeros[0])
+
+#Ordenamiento slicing
+#print(numeros[1:3])
+
+#materia  = "matematicas"
+#Indexado a inversa
+#print(materia[-1])
+#Indexado normal
+#print(materia[10])
+
+#print(materia[2:7])#temat
+#print(materia[2:4])#te
+#print(materia[6:11])#ticas
+
+#diversos=[1, "hola", 3.1415, True]
+#print(diversos)
+
+personal = ("Kevin Mauricio Alvarenga Flores", 28.18)
+print(personal)
