89f01f65bb27de9666168a90bf9bea6206fea5c7
Kevenaxel <hectorsastre476@gmail.com>
2025-07-30 10:24:17 -0600

    new

---
+++ Semana_3/Ejercicio2.py
@@ -0,0 +1,59 @@
+#frutas = ["Manzana", "Banana", "Uva", "Manzana"]
+
+#print(frutas)
+
+#Acceder por indice a Banana
+
+#print(frutas[1]) #Banana
+#frutas.append("Pera")
+#print(frutas)
+#frutas.remove("Manzana")
+#print(frutas)
+
+
+#Recibe un mensaje de agregue a la lista la fruta pera y que solo traiga una manzana
+
+
+
+#Crear una tupla  
+#No se puede editar ni modificar porque es una Tupla.
+#coordenadas = (13.7, -89.2)
+
+#Acceder
+#print(coordenadas[0]) #13.7
+
+
+#Contar y encontrar indice
+#print(coordenadas.count(13.7))
+#print(coordenadas.index(-89.2))
+
+
+#Crear una lista de compras de frutas a comprar en el mercado por unidad,Que incluya: Manzana, banana, Uva, manzana.Solo que te dicen que no puede ser modificada
+
+#frutas = ["Manzana", "Banana", "Uva", "Manzana"]
+
+#print(frutas)
+
+#print(frutas[1]) #Banana
+
+#Recibe un mensaje donde te consultan cuantas frutas traeras
+#print(len(frutas))# Contar el total de frutas.
+
+
+
+#Crear un conjunto de marcas donde te piden que compres 2 zapatos marca NIKE
+# 1 Adidas y 1 Puma
+
+marcas = {"Nike", "Adidas", "Puma", "nike"}
+print(marcas)
+
+#Agregar elementos
+marcas.add("Reebok")
+print(marcas)
+
+#Eliminar elementos
+marcas.discard("Nike")
+print(marcas)
+
+marcas.discard("nike")
+print(marcas)
---
+++ Semana_3/app3.py
@@ -0,0 +1,31 @@
+#Te mandan otro conjunto de datos de marca de zapatos que deseas que chequee si fueron incluidos. (Puma, Reebok, Fila)
+
+marcas = {"Nike", "Adidas", "Puma", "nike"}
+#print(marcas)
+
+#Agregar elementos
+#marcas.add("Reebok")
+#print(marcas)
+
+#Eliminar elementos
+#marcas.discard("Nike")
+#print(marcas)
+
+#marcas.discard("nike")
+#print(marcas)
+
+
+
+
+otros = {"Puma", "Reebok", "Fila"}
+print(otros)
+#print(marcas.union(otros)) #union
+
+#Te piden que hagas las siguientes operaciones:
+#Union de conjuntos, Interseccion de conjuntos, Diferencias de conjuntos.
+print(marcas.intersection(otros))#Interseccion
+print(marcas.difference(otros))
+
+#Tuppla Personal
+nombre = ("Nombre1: Kevin","Nombre2:Mauricio","Apellido1: Alvarenga","Apellido2:Flores","Fecha: 30/07/2025")
+print(nombre)
